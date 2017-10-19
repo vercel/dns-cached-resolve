d8e8eb8db60e9f5ed52ec992129dc07e664c90c3
Olli Vanhoja <olli.vanhoja@gmail.com>
2017-10-19 03:52:53 +0300

    Add test

---
+++ test/test.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const dnsResolve = require('../index.js');
+
+const domain = 's3.amazonaws.com';
+async function run() {
+  console.log(`dnsResolve("${domain}")`);
+  for (let i = 0; i < 10; i++) {
+    console.time('resolve');
+    console.log('IP: ', await dnsResolve(domain));
+    console.timeEnd('resolve');
+  }
+}
+run().catch(console.error);
