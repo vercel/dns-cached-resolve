795f5c47e92dab0598ad139fe9be31d69a92f773
Olli Vanhoja <olli.vanhoja@gmail.com>
2018-08-09 15:21:40 -0700

    Fix tests and use Now as a CI platform for running the tests (#18)

--- test/index.test.js
+++ test/index.test.js
@@ -1,27 +1,49 @@
-const dnsResolve = require('../index.js');
+let dnsResolve;
 const {Resolver} = require('dns')
 
-const domains = ['s3.amazonaws.com', 'zeit.co', 'localhost'];
+const domains = ['s3.amazonaws.com', 'zeit.co'];
 
-async function resolve(name, domain, options = {}) {
+beforeEach(() => {
+  dnsResolve = require('../index.js');
+});
+
+let resnr = 0;
+
+async function resolve(domain, options = {}) {
   console.log(`dnsResolve("${domain}")`);
-  for (let i = 0; i < 10; i++) {
-    console.time('resolve');
-    console.log('IP: ', await dnsResolve(domain, options));
-    console.timeEnd('resolve');
-  }
+
+  const line = `resolve ${resnr++}`;
+  console.time(line);
+  console.log('IP: ', await dnsResolve(domain, options));
+  console.timeEnd(line);
 }
 
-it('should resolve domains', async () => {
+test('should resolve domains', async () => {
   for (const domain of domains) {
     await resolve(domain);
   }
 }, 10000)
 
-it('should resolve using a custom resolver', async () => {
+test('should resolve using a custom resolver', async () => {
   const resolver = new Resolver()
   resolver.setServers(['8.8.8.8', '1.1.1.1'])
   for (const domain of domains) {
     await resolve(domain, { resolver });
   }
 }, 10000)
+
+test('repeated resolves', async () => {
+  for (const domain of domains) {
+    await resolve(domain, domain);
+  }
+}, 100000);
+
+test('concurrent resolves', async () => {
+  for (const domain of domains) {
+    const arr = [domain, domain, domain, domain, domain];
+    const res = await Promise.all(arr.map(resolve));
+    const first = res[0];
+
+    expect(res.every((v) => v === first)).toBeTruthy();
+  }
+}, 10000);
