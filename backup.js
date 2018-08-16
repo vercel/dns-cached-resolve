5ee2153bd572f2fa76d6d2412410e5ab150288ac
Igor Klopov <igor@klopov.com>
2018-08-16 15:30:34 +0300

    backup.js is in-place replacement for index.js. it stores prev value

---
+++ backup.js
@@ -0,0 +1,39 @@
+const dns = require('./');
+const addresses = {};
+
+function startReadingDns (key, host, opts = {}, resolve) {
+  dns(host, opts).then((value) => {
+    addresses[key] = value;
+    // console.log('host', host, 'value', value);
+    if (resolve) resolve(value);
+  }).catch((error) => {
+    error.host = host;
+    error.opts = opts;
+    console.log('not thrown', error);
+  }).then(() => {
+    const t = setTimeout(() => {
+      startReadingDns(key, host, opts);
+    }, 2000);
+
+    if (t.unref) {
+      t.unref();
+    }
+  });
+}
+
+module.exports = function (host, opts = {}) {
+  const key = JSON.stringify({
+    host, ipv6: Boolean(opts.ipv6)
+  });
+
+  if (addresses[key]) {
+    return addresses[key];
+  }
+
+  const p = new Promise((resolve) => {
+    startReadingDns(key, host, opts, resolve);
+  });
+
+  addresses[key] = p;
+  return p;
+};
